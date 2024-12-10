7aee8a6575d6a4e2b034bbe7eda1bdbe3beebcdf
Tani Kaye <1tanikaye@gmail.com>
2024-12-10 16:07:21 -0500

    Initial commit for Fee Optimizer

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Fee Optimizer</h1>
+      <nav>
+        <ul>
+          <li><a href="#track-fees">Track Fees</a></li>
+          <li><a href="#optimize">Optimize</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <section id="track-fees">
+        <h2>Track Your Transaction Fees</h2>
+        <p>Enter your transaction details to track and optimize your fees.</p>
+        {/* We'll add more functionality here later */}
+      </section>
+    </main>
+  );
+}
+
+export default Main;
